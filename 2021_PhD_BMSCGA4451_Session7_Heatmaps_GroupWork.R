8599bed0cd49e3ec1e368e5025d635e813a4e88e
fredlapolla <fred.lapolla@med.nyu.edu>
2021-10-12 12:47:47 -0400

    Update 2021_PhD_BMSCGA4451_Session7_Heatmaps_GroupWork.R

--- 2021_PhD_BMSCGA4451_Session7_Heatmaps_GroupWork.R
+++ 2021_PhD_BMSCGA4451_Session7_Heatmaps_GroupWork.R
@@ -2,7 +2,7 @@
 
 dolgalevNormRNA <- read.csv("https://raw.githubusercontent.com/fredlapolla/VilcekR_fall21/main/counts.normalized.csv")
 
-#1. Convert the following data into a matrix, with rownames for the genes being named. Re-order the columns to group WT and KO mice strains together. Make subsets of highly expressed genes (choose your own threshold) 
+#1. Convert the data into a matrix, with rownames for the genes being named. Re-order the columns to group WT and KO mice strains together. Make subsets of highly expressed genes (choose your own threshold) 
 
 
 #2. Try making a heatmap of the data you subsetted in part 1. Try both clustering by column and setting this setting to false. Explore the help information and see what other row clustering methods are available
